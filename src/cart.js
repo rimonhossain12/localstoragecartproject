47b54abe38de90d9eca0de447e1a63e3ad0bf18f
rimonhossain12 <rimonmpi@gmail.com>
2022-11-25 18:59:50 +0600

    shoping cart increment and decrement added

--- src/cart.js
+++ src/cart.js
@@ -8,8 +8,9 @@ const generateItemCart = () => {
   return (shoppingCart.innerHTML = basket.map((x) => {
     // find all products id and item;
     const { id, item } = x;
-    const searchData = shopItemsData.find((x) => x.id === id);
-    const { name, img, price } = searchData;
+    const searchData = shopItemsData.find((x) => x.id === id) || [];
+    const {name, img, price,title } = searchData;
+    // console.log(searchData);
     return `
         <div class="cart-item">
             <img width="100" src="${img}" alt="not found" />
@@ -22,9 +23,9 @@ const generateItemCart = () => {
                 <i class="bi bi-x-lg"></i>
             </div>
             <div class="buttons">
-                <i onclick="decrement()" class="bi bi-dash"></i>
-                <div id="update-">10</div>
-                <i onclick="increment()" class="bi bi-plus"></i>
+                <i onclick="decrement(${id})" class="bi bi-dash"></i>
+                <div id="update-${id}">${item}</div>
+                <i onclick="increment(${id})" class="bi bi-plus"></i>
             </div>
             </div>
     </div>
@@ -33,6 +34,39 @@ const generateItemCart = () => {
 };
 
 generateItemCart();
+
+const increment = (id) => {
+  const selectedId = id;
+  const search = basket.find((x) => x.id === selectedId);
+  if (search === undefined) {
+    basket.push({
+      id: selectedId,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  updateQuantity(selectedId);
+  localStorage.setItem("order", JSON.stringify(basket));
+};
+
+const decrement = (id) => {
+    const selectedId = id;
+    const search = basket.find((x) => x.id === selectedId);
+    if(search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    updateQuantity(selectedId);
+    localStorage.setItem('order',JSON.stringify(basket));
+}
+
+const updateQuantity = (id) => {
+   const search = basket.find((x) => x.id === id);
+   document.getElementById(`update-${id}`).innerHTML = search.item;
+
+};
 
 const cartQuantity = () => {
   const search = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
